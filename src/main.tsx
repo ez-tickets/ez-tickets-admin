f95c4a3cb3bb6be1385ae6888eda68d12131608b
CHOBI <139092470+CHOBI-project@users.noreply.github.com>
2024-11-25 10:52:44 +0900

    :recycle: - `ProductRegistration` component.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import Admin from "@/admin/Admin.tsx";
 import CategoryList from "@/admin/screen/Category/CategoryList.tsx";
 import Home from "@/admin/screen/Home/Home.tsx";
 import ProductList from "@/admin/screen/Product/ProductList.tsx";
-import ProductRegister from "@/admin/screen/Register/ProductRegister.tsx";
+import ProductRegister from "@/admin/screen/Register/Prod/ProductRegister.tsx";
 import TitleBar from "@/application/TitleBar.tsx";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
