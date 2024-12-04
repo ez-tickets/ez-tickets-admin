d7babe2a4970d0be39ef44e5476df733796fa25c
CHOBI <139092470+CHOBI-project@users.noreply.github.com>
2024-12-04 15:16:41 +0900

    :truck: - rename `Product` to `product`.

---
+++ src/admin/screen/catalog/category/register/CategoryRegister.tsx
@@ -0,0 +1,42 @@
+import CategoryActionButton from "@/admin/screen/catalog/category/register/components/CategoryActionButton.tsx";
+import CategoryName from "@/admin/screen/catalog/category/register/components/CategoryName.tsx";
+import { useCategoryRegistrationStore } from "@/admin/store/RegistrationStore.ts";
+import { debug } from "@/admin/store/action/ProdRegistrationAction.ts";
+import Header from "@/parts/Header.tsx";
+import { Fragment, useState } from "react";
+
+function CategoryRegister() {
+  const [categoryName, setCategoryName] = useState<string>("");
+  // debug用
+  const { categoryRegisterDispatcher } = useCategoryRegistrationStore();
+
+  return (
+    <Fragment>
+      <Header
+        title={"カテゴリー登録"}
+        element={
+          <Fragment>
+            {/* debug */}
+            <button
+              type={"button"}
+              onClick={() => categoryRegisterDispatcher(debug())}
+            >
+              登録
+            </button>
+          </Fragment>
+        }
+      />
+
+      <CategoryName
+        categoryName={categoryName}
+        setCategoryName={setCategoryName}
+      />
+      <CategoryActionButton
+        categoryName={categoryName}
+        setCategoryName={setCategoryName}
+      />
+    </Fragment>
+  );
+}
+
+export default CategoryRegister;
