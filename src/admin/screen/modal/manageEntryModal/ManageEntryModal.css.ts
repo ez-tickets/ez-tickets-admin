1c770ab8c5042a1980f46aab1bfe3445a7e923f2
CHOBI <139092470+CHOBI-project@users.noreply.github.com>
2024-12-16 15:45:55 +0900

    :sparkles: - toastify add product.

--- src/admin/screen/modal/manageEntryModal/ManageEntryModal.css.ts
+++ src/admin/screen/modal/manageEntryModal/ManageEntryModal.css.ts
@@ -1,14 +1,22 @@
 import { style } from "@vanilla-extract/css";
 
 export const manageEntryModal = {
-  title: style({
-    padding: "10px 20px 0",
-  }),
-
   headerContainer: style({
+    position: "sticky",
+    width: "100%",
+    height: "60px",
+    top: 0,
+    left: 0,
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
+    backgroundColor: "white",
+    boxShadow: "1px 2px 3px lightgray",
+    zIndex: 100,
+  }),
+
+  title: style({
+    padding: "0 20px",
   }),
 
   closeIcon: style({
@@ -35,26 +43,6 @@ export const manageEntryModalContainer = {
     width: "75%",
     height: "90%",
     padding: 0,
-    backgroundColor: "#fff",
-  },
-};
-
-export const selectModal = {
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    backgroundColor: "rgba(0,0,0,0.4)",
-    zIndex: 100,
-  },
-  content: {
-    position: "relative",
-    top: "52%",
-    left: "60%",
-    transform: "translate(-50%, -50%)",
-    width: "30%",
-    height: "40%",
-    padding: "0",
     backgroundColor: "#fff",
   },
 };
