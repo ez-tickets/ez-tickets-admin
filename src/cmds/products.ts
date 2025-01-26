4fa7023192d6a545a03e44e4504324dc126f92e5
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-01-27 05:36:31 +0900

    :hammer: - Define IPC functions with the frontend.

--- src/cmds/products.ts
+++ src/cmds/products.ts
@@ -3,28 +3,42 @@
 import { invoke } from "@tauri-apps/api/core";
 
 export type Product = {
-  id: string;
-  name: string;
-  category: string | null;
-  desc: string;
-  price: number;
-  path: string;
-  available: boolean;
-  order: number;
+  id: string,
+  name: string,
+  price: number,
+}
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  return await invoke<Product[]>("get_products");
 };
 
-//todo: categoryIDを使ってfetchProductsを呼び出す
-export const fetchProducts = async (): Promise<Product[]> => {
-  return await invoke<Product[]>("products");
+export type ProductInCategory = {
+  id: string;
+  name: string;
+  ordering: number;
+}
+
+export const fetchProductsInCategory = async (id: string): Promise<ProductInCategory[]> => {
+  return await invoke<ProductInCategory[]>("get_product_in_category", { id: id });
 };
+
+
+type ProductDetails = {
+  id: string;
+  name: string;
+  desc: string;
+  price: number;
+}
+
+export const fetchProductDetails = async (id: string): Promise<ProductDetails> => {
+  return await invoke<ProductDetails>("get_product_details", { id: id });
+}
 
 export type RegisterProduct = {
   name: string;
-  category: string | null;
   desc: string;
   price: number;
   path: string;
-  available: boolean;
 };
 
 export const registerProduct = async (
@@ -34,13 +48,12 @@ export const registerProduct = async (
 };
 
 export type UpdateProduct = {
-  name: string;
-  category: string | null;
-  desc: string;
-  price: number;
+  name?: string;
+  desc?: string;
+  price?: number;
   path?: string;
-  available: boolean;
 };
+
 
 export const updateProduct = async (
   id: string,
@@ -52,3 +65,16 @@ export const updateProduct = async (
 export const deleteProduct = async (id: string): Promise<void> => {
   await invoke<void>("delete_product", { id: id });
 };
+
+
+export type OrderedProduct = {
+  id: string;
+  ordering: number;
+};
+
+export const reorderProducts = async (
+  category_id: string,
+  reorder: OrderedProduct[]
+): Promise<void> => {
+  await invoke<void>("change_ordering_product_in_category", { id: category_id, new: reorder });
+}
